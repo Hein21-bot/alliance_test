6b2a06fed367edf03b298745c0af46f25ed099b2
Kumo <heinhtet25944.com>
2022-11-21 17:14:49 +0630

    SSC

---
+++ src/components/payroll/sscReportforGovernment.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+
+class AttendanceType extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+}
+
+
+render(){
+    return(
+        <div> <table
+        width="99%"
+        className="table table-striped table-bordered table-hover table-responsive nowrap dt-responsive"
+        id="dataTables-table"
+      /></div>
+    );}
+}
+
+export default AttendanceType;
