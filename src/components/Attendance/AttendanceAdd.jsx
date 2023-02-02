a602a705216cbf027d875c5be72cf88fa2f088c4
Kumo <heinhtet25944.com>
2023-02-02 13:02:36 +0630

    attAdd

--- src/components/Attendance/AttendanceAdd.jsx
+++ src/components/Attendance/AttendanceAdd.jsx
@@ -14,24 +14,48 @@ export default class AttendanceAdd extends Component{
     componentDidMount(){
        
     }
+     success(data) { console.log(data);
+        const crd = data.coords;
+      this.setState({
+        latti: crd.latitude,
+        longi: crd.longitude
+      })
+
+      }
    handleClick(){
-    this.getGeolocation()
-   }
-   getGeolocation(){
-    if(navigator.geolocation){
-        console.log("ssss");
-        navigator.geolocation.getCurrentPosition((position)=>{ console.log(position.coords.latitude);
-            this.setState({
-                latti:position.coords.latitude,
-                longi:position.coords.longitude
-            })
-        })
-    }
-   }
-
-
+    // this.getGeolocation()
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
       
+      
+      function error(err) {
+        alert(`ERROR(${err.code}): ${err.message}`);
+      }
+     
+      navigator.geolocation.getCurrentPosition(this.success.bind(this), error, options);
+   
+   }
+//    getGeolocation(){
     
+//     if(navigator.geolocation){
+//         console.log("ssss");
+       
+//         navigator.geolocation.getCurrentPosition((position)=>{ console.log(position.coords.latitude);
+//             this.setState({
+//                 latti:position.coords.latitude,
+//                 longi:position.coords.longitude
+//             })
+//         },(err)=>{
+//             alert(err.message)
+//         })
+//     } else{
+//         alert('Location is not Support on your browser')
+//     }
+//    }
+
     render(){ console.log(this.state.latti);
         return(
             <div>
